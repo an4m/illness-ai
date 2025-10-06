7d51567189824fe2a156eba9c57a9002b55ad099
an4m <anam.vasa@gmail.com>
2025-10-06 23:50:29 +0100

    Add @vercel/speed-insights package and integrate SpeedInsights component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      {process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' && <SpeedInsights />}
     </html>
   );
 }
